37212fb839e195859c74e0718a072f2584ff02b6
Albedo <masfuadfotobersama19@gmail.com>
2025-02-05 17:21:32 +0700

    first commit

---
+++ lib/posts.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'http_service.dart';
+import 'post_model.dart';
+import 'post_detail.dart';
+
+class PostsPage extends StatelessWidget {
+  final HttpService httpService = HttpService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Posts"),
+      ),
+      body: FutureBuilder(
+        future: httpService.getPosts(),
+        builder: (BuildContext context, AsyncSnapshot<List<Post>> snapshot) {
+          if (snapshot.hasData) {
+            List<Post> posts = snapshot.data!;
+            return ListView(
+              children: posts.map((Post post) {
+                return Card(
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ListTile(
+                            onTap: () => Navigator.of(context).push(
+                                MaterialPageRoute(
+                                    builder: (context) => PostDetail(
+                                        post: post,
+                                    ),
+                                ),
+                            ),
+                            leading: Icon(Icons.album),
+                            title: Text(post.title),
+                            subtitle: Text(post.body),
+                        ),
+                        Text(
+                          post.title,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          "User ID: ${post.userId}",
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }).toList(),
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
